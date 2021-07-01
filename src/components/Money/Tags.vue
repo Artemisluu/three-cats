f5fc4d4be8e1a9b6368909085e2430098e429941
Artemisluu <1160587518@qq.com>
2021-07-01 21:00:21 +0800

    可以选中或取消tag

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="tags">
     <div class="icon-list">
-      <div v-for="item in items" :key="item" class="icon-list-item">
+      <div
+          v-for="item in items"
+          :key="item"
+          @click="toggle(item)"
+          :class="['icon-list-item', {selected:selectedTags.indexOf(item)>=0}]"
+      >
         <Icon :name="item"/>
         {{ item }}
       </div>
@@ -12,29 +17,40 @@
 
 <script lang="ts">
 
-export default {
-  name: 'Tags',
-  data() {
-    return {
-      items: [
-        '保险',
-        '医疗',
-        '宠物',
-        '家居',
-        '家电',
-        '服装',
-        '机票',
-        '游戏',
-        '生鲜',
-        '电影',
-        '维修',
-        '美妆',
-        '美食',
-        '酒店',
-      ]
-    };
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Tags extends Vue {
+  items: string[] = [
+    '保险',
+    '医疗',
+    '宠物',
+    '家居',
+    '家电',
+    '服装',
+    '机票',
+    '游戏',
+    '生鲜',
+    '电影',
+    '维修',
+    '美妆',
+    '美食',
+    '酒店',
+  ];
+  selectedTags: string[] = [];
+
+  toggle(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tag);
+    }
   }
-};
+}
+
+
 </script>
 
 <style lang="scss" scoped>
@@ -44,6 +60,7 @@ export default {
   flex-wrap: wrap;
   flex-direction: column-reverse;
   padding: 25px;
+  flex-grow: 1;
 
   .icon-list {
     font-size: 14px;
@@ -60,6 +77,14 @@ export default {
       flex-direction: column;
       justify-content: space-around;
       align-items: center;
+
+      &.selected {
+        color: #FF6C39;
+
+        .icon {
+          fill: #FF6C39;
+        }
+      }
 
       .icon {
         width: 36px;
